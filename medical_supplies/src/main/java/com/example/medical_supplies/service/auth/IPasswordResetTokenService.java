41019611ccf2c50854f8677618c934d21e2c0328
dinhnam72 <dinhnam20162011@gmail.com>
2024-01-10 15:01:18 +0700

    update 15h00 10/1

---
+++ medical_supplies/src/main/java/com/example/medical_supplies/service/auth/IPasswordResetTokenService.java
@@ -0,0 +1,11 @@
+package com.example.medical_supplies.service.auth;
+
+import com.example.medical_supplies.model.auth.Account;
+
+
+public interface IPasswordResetTokenService {
+//    void createPasswordResetTokenForUser(PasswordResetToken passwordResetToken);
+    Boolean existsByToken(String token);
+//    PasswordResetToken getPasswordResetToken(String token);
+//    PasswordResetToken getPasswordResetToken(Account account);
+}
---
+++ medical_supplies/src/main/java/com/example/medical_supplies/service/impl/MyUserDetailService.java
@@ -0,0 +1,33 @@
+package com.example.medical_supplies.service.impl;
+
+import com.example.medical_supplies.model.auth.Account;
+import com.example.medical_supplies.model.auth.MyUserDetail;
+import com.example.medical_supplies.repository.auth.IAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * This class implements the UserDetailsService interface for loading user details.
+ */
+@Service
+public class MyUserDetailService implements UserDetailsService {
+    @Autowired
+    private IAccountRepository accountRepository;
+
+    /**
+     * This method loads user details by email.
+     * @author: NamND
+     * @date: 10/01/2024
+     * @param email The email.
+     * @return The user details of the specified username.
+     * @throws UsernameNotFoundException If the username is not found.
+     */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Email " + email + " không tồn tại"));
+        return new MyUserDetail(account);
+    }
+}
