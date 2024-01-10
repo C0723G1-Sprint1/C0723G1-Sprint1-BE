1d15dd200a8ec4d26bfd3f9a7367617b9c27cfb8
HoNgocAn <anhongoc1306@gmail.com>
2024-01-10 16:02:34 +0700

    an up

--- medical_supplies/src/main/java/com/example/medical_supplies/controller/product/ProductController.java
+++ medical_supplies/src/main/java/com/example/medical_supplies/controller/product/ProductController.java
@@ -24,19 +24,19 @@ public class ProductController {
     @Autowired
     private IProductService iProductService;
 
-    @GetMapping("/list/product")
-    public ResponseEntity<?> getAllProduct(
-            @RequestParam(name = "nameCustomer", defaultValue = "", required = false) String name,
-            @RequestParam(name = "typeCustomer", defaultValue = "", required = false) String typeCustomer,
-            @RequestParam(name = "typeCustomer", defaultValue = "", required = false) String typeCustomer,
-            @RequestParam(defaultValue = "0", required = false) int page
-    ) {
-        Pageable pageable = PageRequest.of(page, 5);
-        List<Products> customerDto = iProductService.findAllProduct();
-        if (customerDto.isEmpty()) {
-            return new (HttpStatus.NO_CONTENT);
-        } else {
-            return new ResponseEntity<>(customerDto, HttpStatus.OK);
-        }
-    }
+//    @GetMapping("/list/product")
+//    public ResponseEntity<?> getAllProduct(
+//            @RequestParam(name = "nameCustomer", defaultValue = "", required = false) String name,
+//            @RequestParam(name = "typeCustomer", defaultValue = "", required = false) String typeCustomer,
+//            @RequestParam(name = "typeCustomer", defaultValue = "", required = false) String typeCustomer,
+//            @RequestParam(defaultValue = "0", required = false) int page
+//    ) {
+//        Pageable pageable = PageRequest.of(page, 5);
+//        List<Products> customerDto = iProductService.findAllProduct();
+//        if (customerDto.isEmpty()) {
+//            return new (HttpStatus.NO_CONTENT);
+//        } else {
+//            return new ResponseEntity<>(customerDto, HttpStatus.OK);
+//        }
+//    }
 }
